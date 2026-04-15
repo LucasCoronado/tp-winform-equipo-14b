44ead9d4b2e244ee9e1ea0ec032d659fd55e5cdd
LucasCoronado <coronadolucasm@gmail.com>
2026-04-14 23:31:25 -0300

    Creación clase AccesoDatos

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader? lector;
+
+        public SqlDataReader? Lector
+        {
+            get { return lector; } 
+        }
+
+        public AccesoDatos()
+        {
+            string cadenaConexion = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+            conexion = new SqlConnection(cadenaConexion);
+            comando = new SqlCommand();
+        }
+
+        public void SetearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+
+    }
+}
